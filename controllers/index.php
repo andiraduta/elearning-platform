476e27eacae9cfcc55ca8e1259a4a6c605213da6
andiraduta <andyradutza@ymail.com>
2013-05-10 00:52:58 +0300

    controllers

---
+++ controllers/index.php
@@ -0,0 +1,16 @@
+<?php
+
+class Index extends Controller {
+	
+    public $u;
+    
+	function __construct() {
+		parent::__construct();        
+	}
+	
+	function index() {
+        $data = array();
+		//$this->view->render('index', $data, false);
+	}
+	
+}
---
+++ controllers/login.php
@@ -0,0 +1,34 @@
+<?php
+
+class Login extends Controller {
+
+	public function __construct() {
+		parent::__construct();
+	}
+	
+	public function index() {
+		$data = array();
+		if( isset($_POST['login']) ) {
+			if( trim($_POST['utilizator']) != "" && trim($_POST['parola']) != "" ) {
+				$this->incarcaModel('login');
+				if( $this->login->validare_login($_POST['utilizator'], $_POST['parola']) ) {
+					// redirect in prima pagina a aplicatiei
+					header("Location: " . URL . "index.php?url=index");
+					exit();
+				} else {
+					$data['msg'] = '<div class="alert alert-error">Datele introduse nu sunt corecte!</div>';
+				}
+			} else {
+				$data['msg'] = '<div class="alert alert-error">Va rugam sa completati toate campurile text!</div>';
+			}
+		}
+		$this->view->render('login/index', $data);
+	}
+	
+	public function iesire() {
+		session_destroy();
+		header("Location: " . URL . "index.php?url=login");
+		exit;
+	}
+
+} 
