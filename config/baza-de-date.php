bf1b2fe7a5bd2dc58e2ff864062bfb0f22e228a9
andiraduta <andyradutza@ymail.com>
2013-05-10 00:51:23 +0300

    configuratii

---
+++ config/baza-de-date.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'elearning');
+define('DB_USER', 'root');
+define('DB_PASS', '');
---
+++ config/constante.php
@@ -0,0 +1,6 @@
+<?php
+ini_set('display_errors', 'On');
+
+define( 'URL', 'http://localhost:8080/elearning/' );
+
+define( 'LIBS', 'libs/' );
