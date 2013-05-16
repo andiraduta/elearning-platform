77b65c523c89ecbbdadbe9634a6e43401c2cde52
andiraduta <andyradutza@ymail.com>
2013-05-17 00:50:44 +0300

    template

--- views/index.php
+++ views/index.php
@@ -5,4 +5,20 @@
 				<p><a href="#" class="btn btn-primary btn-large">Vezi lista de cursuri &raquo;</a></p>
 			</div>
 
-	
+			<div class="row">
+				<div class="span8">
+					<h3>Categorii cursuri</h3>
+					<ul class="nav nav-pills nav-stacked">
+						<li>
+							<a href="#">Facultatea de Matematica si Informatica</a>
+							<ul>
+								<li><a href="#">Departamentul de Matematica</a></li>
+								<li><a href="#">Departamentul de Informatica</a></li>
+								<li><a href="#">Departamentul de Tehnologii</a></li>
+							</ul>
+						</li>
+						<li><a href="#">Facultatea de Geografie</a></li>
+						<li><a href="#">Facultatea de Litere</a></li>
+					</ul>
+				</div>
+			</div>
