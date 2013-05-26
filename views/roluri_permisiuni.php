c501f4b8c17a82aa30c0f1341b9c0ed659b665f1
andiraduta <andyradutza@ymail.com>
2013-05-27 01:09:36 +0300

    template

---
+++ views/roluri_permisiuni.php
@@ -0,0 +1,46 @@
+
+	<div class="row">
+	
+		<div class="span8">
+		
+			<h3>Roluri si permisiuni</h3>
+				<p>
+					<a href="#" class="btn btn-success"><i class="icon-plus icon-white"></i> Adauga rol utilizator</a>
+				</p>
+				<br />
+				<table class="table table-bordered table-striped">
+					<thead>
+						<tr>
+							<th>Id</th>
+							<th>Nume rol</th>
+							<th width="200">Actiuni</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>1</td>
+							<td>administrator</td>
+							<td>
+								<a href="#" class="btn btn-info"><i class="icon-pencil icon-white"></i> Permisiuni</a> 
+							</td>
+						</tr>
+						<tr>
+							<td>2</td>
+							<td>programator</td>
+							<td>
+								<a href="#" class="btn btn-info"><i class="icon-pencil icon-white"></i> Permisiuni</a> 
+							</td>
+						</tr>
+						<tr>
+							<td>3</td>
+							<td>client</td>
+							<td>
+								<a href="#" class="btn btn-info"><i class="icon-pencil icon-white"></i> Permisiuni</a> 
+							</td>
+						</tr>
+					</tbody>
+				</table>
+		
+		</div>
+	
+	</div>
