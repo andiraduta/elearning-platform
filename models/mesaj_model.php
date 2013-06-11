e34c9c418a4d617e94b222e1393c4a6ace54aa3a
unknown <andyradutza@ymail.com>
2013-06-11 15:11:40 +0300

    models

--- models/mesaj_model.php
+++ models/mesaj_model.php
@@ -7,7 +7,61 @@ class Mesaj_Model extends Model {
 	}
 	
 	public function lista_mesaje($id_utilizator) {
+		$sql = "SELECT id_mesaj, status, subiect, mesaj, uu.nume, uu.username 
+			FROM `mesaje` m 
+			INNER JOIN `utilizatori` u ON m.catre_utilizator = u.id_utilizator 
+			INNER JOIN `utilizatori` uu ON m.de_la_utilizator = uu.id_utilizator
+			WHERE m.catre_utilizator = ".(int) $id_utilizator."
+			ORDER BY id_mesaj DESC;";
+		$query = mysql_query($sql);
+		$array = array();
+		while( $row = mysql_fetch_assoc($query) ) {
+			$array[] = $row;
+		}
+		
+		return $array;
+	}
 	
+	public function detalii_mesaj($id_mesaj) {
+		$sql = "SELECT id_mesaj, status, subiect, mesaj, m.data_creare, uu.nume, uu.username 
+			FROM `mesaje` m 
+			INNER JOIN `utilizatori` uu ON m.de_la_utilizator = uu.id_utilizator
+			WHERE m.id_mesaj = ".(int) $id_mesaj." LIMIT 1;";
+		$query = mysql_query($sql);
+		return mysql_fetch_assoc($query);
+	}
+	
+	public function lista_mesaje_trimise($id_utilizator) {
+		$sql = "SELECT id_mesaj, status, subiect, mesaj, m.data_creare, nume, username 
+			FROM `mesaje` m 
+			INNER JOIN `utilizatori` u ON m.catre_utilizator = u.id_utilizator 
+			WHERE m.de_la_utilizator = ".(int) $id_utilizator."
+			ORDER BY id_mesaj DESC;";
+		$query = mysql_query($sql);
+		$array = array();
+		while( $row = mysql_fetch_assoc($query) ) {
+			$array[] = $row;
+		}
+		
+		return $array;
+	}
+	
+	public function marcheaza_citit($id_mesaj) {
+		$sql = "UPDATE mesaje SET status = 1 WHERE id_mesaj = ".(int) $id_mesaj;
+		if( mysql_query($sql) ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public function sterge_mesaj($id_mesaj) {
+		$sql = "DELETE FROM mesaje WHERE id_mesaj = ".$id_mesaj;
+		if( mysql_query($sql) ) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
 	public function adauga_mesaj($data) {
